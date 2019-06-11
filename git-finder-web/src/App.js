c211fc541c458f96f01325924cddb7c8bd7f916f
vitorfrs-dev <vitorfrs98@gmail.com>
2019-06-10 21:22:03 -0300

    alterar entre a visualização dos componentes

--- git-finder-web/src/App.js
+++ git-finder-web/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReposContainer from './components/ReposContainer';
 import Navbar from './components/Navbar';
 import FavoriteReposContainer from './components/FavoriteReposContainer';
+import { Button } from '@material-ui/core';
+import ViewContainer from './components/ViewContainer';
 
 function App() {
+  
   return (
     <div>
        <Navbar />
-        <ReposContainer />
-        <FavoriteReposContainer />
+       <ViewContainer />
     </div>
     
   );
---
+++ git-finder-web/src/components/ViewContainer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReposContainer from './ReposContainer';
+import FavoriteReposContainer from './FavoriteReposContainer';
+import { Button } from '@material-ui/core';
+
+
+class ViewContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            view: 'search'
+        }
+        this.changeView = this.changeView.bind(this);
+    }
+
+    changeView() {
+
+        let newView = this.state.view === 'search' ? 'favorite': 'search';
+
+        this.setState({
+            view: newView
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <Button onClick={this.changeView}>
+                        Ver {this.state.view == 'search'? 'favoritos' : 'Repositórios'}
+                    </Button>
+                </div>
+
+                {this.state.view === 'search' && <ReposContainer />}
+                {this.state.view === 'favorite' && <FavoriteReposContainer />}
+            </div>
+        );
+    }
+}
+
+export default ViewContainer;
